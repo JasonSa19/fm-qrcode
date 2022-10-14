9f463575018b43fdd1c55203ad71189964dadeab
Jason Sauerwald <js@kreativkarussell.de>
2022-10-14 13:30:56 +0200

    delete placeholder

--- src/Main.js
+++ src/Main.js
@@ -1,22 +1,7 @@
-import logo from "./logo.svg";
-
 function Main() {
   return (
     <div className="Main">
-      <header className="Main-header">
-        <img src={logo} className="Main-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="Main-header"></header>
     </div>
   );
 }
